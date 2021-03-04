d3f55c9eecb9226fe1ffcec0fd0803b95323f665
Something270 <55809521+Something270@users.noreply.github.com>
2021-03-04 10:17:34 -0700

    Added basic files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace personas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
